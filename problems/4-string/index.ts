11c130a607966223627a1589222ab042d0aec396
AmirAhmadzadeh <ahamadzadeh1378@gmail.com>
2021-12-10 23:34:06 +0330

    restructure dirs

--- problems/4-string/index.ts
+++ problems/4-string/index.ts
@@ -1,12 +1,11 @@
 /// problem  4 : Strings
 
-//// how abstract thinking comes with the strings ?
-//// desc : we are given to strings T and S , return when they are equal
-//// when both are typed out Any hashtag that appears in the string
-//// counts as a backspace
-//
+// how abstract thinking comes with the strings ?
+// desc : we are given to strings T and S , return when they are equal
+// when both are typed out Any hashtag that appears in the string
+// counts as a backspace
 //  example : axeqq#a  asexq#a
-//  :         axeqa  ===  asexa result : false
+//            axeqa  ===  asexa result : false
 //  example-2  : axeqq#a  axeqW#a
 //  solution-2 : axeqa  axeqa => res true ;
 
@@ -25,13 +24,31 @@ function generateString(str: string): string {
 function compareTwoString(S: string, T: string) {
   let SRes: string = generateString(S);
   let TRes: string = generateString(T);
-  //this is not a good way to compare
+
   return SRes === TRes;
 }
 
-console.log(compareTwoString("ax#c", "amir is g#x"));
-console.log(compareTwoString("ax#c", "ag#c"));
+//console.log(compareTwoString("ax#c", "amir is g#x"));
+//console.log(compareTwoString("ax#c", "ag#c"));
 
-console.log(
-  "/////////////////////////////optimized solutions////////////////////"
-);
+const rl = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function ask(msg: string) {
+  return new Promise((resolve) => {
+    return rl.question(msg, resolve);
+  });
+}
+
+(async function main() {
+  let S = (await ask("Enter S : ")) as string;
+  let T = (await ask("Enter T : ")) as string;
+  if (compareTwoString(S as string, T as string)) {
+    console.log("locky");
+  } else {
+    console.log("not locky");
+  }
+  rl.close();
+})();
