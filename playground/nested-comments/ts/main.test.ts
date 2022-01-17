1f1b4826c83eff9cd146f9fd812653b264103982
AmirAhmadzadeh <ahamadzadeh1378@gmail.com>
2022-01-17 20:23:55 +0330

    refactor :remove logs and wrong solution

--- playground/nested-comments/ts/main.test.ts
+++ playground/nested-comments/ts/main.test.ts
@@ -4,21 +4,15 @@ import {
   smallComments,
   reshapedSmallComments,
 } from "./comments.po";
-import { reshapeComments, logRecursively, findAndReshape } from "./main";
+import { findAndReshape } from "./main";
 
 describe("test", () => {
-  xit("should pass", () => {
-    const result = reshapeComments(comments);
-    expect(result).toEqual(reshapedComments);
-  });
-
   it("reshape comments second solution", () => {
     const result = findAndReshape(comments);
     expect(result).toEqual(reshapedComments);
   });
 
   it("reshape comments second solution with array of 3 elements", () => {
-    console.log("😄");
     const result = findAndReshape(smallComments);
     expect(result).toEqual(reshapedSmallComments);
   });
--- playground/nested-comments/ts/main.ts
+++ playground/nested-comments/ts/main.ts
@@ -4,37 +4,6 @@ export type CommentType = {
   parentId?: string;
 };
 
-export function reshape(items: CommentType[]) {
-  //
-}
-
-export function reshapeComments(items: any[]) {
-  let comments = [...items];
-  const shouldBeRemove: string[] = [];
-
-  for (let i = 0; i < comments.length; ++i) {
-    let parent = comments[i];
-    for (let j = 0; j < comments.length; ++j) {
-      let child = comments[j];
-      if (child.parentId === parent.id) {
-        //reshapeComments(comments, child.id);
-        if (parent.children) {
-          parent.children.push(child);
-        } else {
-          parent.children = [child];
-        }
-        //comments.splice(j, 1);
-      }
-    }
-  }
-
-  comments = comments.filter(
-    (item: CommentType) => !shouldBeRemove.includes(item.id)
-  );
-
-  return comments;
-}
-
 export function reshapeChildren(
   items: CommentType[],
   parent: CommentType,
@@ -65,16 +34,6 @@ export function findAndReshape(comments: CommentType[]) {
   for (let i = 0; i < comments.length; ++i) {
     let children = reshapeChildren(comments, comments[i]);
   }
-  console.log(JSON.stringify(comments, null, 2));
-  return comments;
-}
 
-export function logRecursively(comments: CommentType[]) {
-  for (let i = 0; i < comments.length; ++i) {
-    const comment = comments[i];
-    console.log(comment);
-    if (comment.children) {
-      logRecursively(comment.children);
-    }
-  }
+  return comments;
 }
