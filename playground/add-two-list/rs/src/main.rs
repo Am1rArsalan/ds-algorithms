54bcadf62c5c08710f3fda2c9067f36bc7d52a23
AmirAhmadzadeh <ahamadzadeh1378@gmail.com>
2022-01-27 20:06:58 +0330

    wip : add two list problem in rust

--- playground/add-two-list/rs/src/main.rs
+++ playground/add-two-list/rs/src/main.rs
@@ -1,34 +1,98 @@
-fn main() {
-    println!("make number test {:?}", makeNumber(vec![1, 2, 3, 4]));
-}
-
-fn make_number(numbers: Vec<i64>) -> i64 {
-    let mut result = 0;
-    for (current_index, number) in numbers.iter().enumerate() {
-        result += number * i64::pow(10, current_index.try_into().unwrap());
-    }
-
-    result
-}
-
+#[derive(Debug, std::clone::Clone)]
 struct ListNode {
-    value: i32,
-    next: Box<Option<ListNode>>,
+    pub val: usize,
+    pub next: Option<Box<ListNode>>,
 }
 
-fn generate_number_from_list(list: ListNode) {}
-
-fn add_two_number(l1: Option<ListNode>, l2: Option<ListNode>) -> Result<i64, String> {
-    let result_one = vec![];
-    let result_two = vec![];
-
-    Ok(3232)
+impl ListNode {
+    #[inline]
+    pub fn new(val: usize) -> Self {
+        Self { val, next: None }
+    }
 }
 
-fn check_list(l1: Option<ListNode>) -> Option<String> {
-    if l1.is_none() {
-        return Some(String::from("the list is not valid"));
+fn make_number_from_list(list: Option<Box<ListNode>>) -> usize {
+    match list {
+        None => 0,
+        Some(temp) => {
+            let cloned_list = temp.clone();
+            let mut result = "".to_owned();
+            result = format!("{}{}", result, cloned_list.val);
+            let mut iter_cloned_list = cloned_list.next;
+            while let Some(iter_cloned_value) = iter_cloned_list {
+                result = format!("{}{}", iter_cloned_value.val, result);
+                iter_cloned_list = iter_cloned_value.next;
+            }
+
+            println!("result number in string mode {} ", result);
+            let result: usize = result.parse().unwrap();
+
+            result
+        }
+    }
+}
+
+fn make_list_from_array(list_array: Vec<usize>) -> Option<Box<ListNode>> {
+    if list_array.len() <= 0 {
+        return None;
     }
 
-    None
+    let mut list: Option<Box<ListNode>> = None;
+    let mut ref_list = &mut list;
+
+    for item in list_array {
+        if let Some(_) = ref_list {
+            while let Some(node_value) = ref_list {
+                ref_list = &mut node_value.next;
+            }
+            *ref_list = Some(Box::new(ListNode::new(item)));
+        } else {
+            *ref_list = Some(Box::new(ListNode::new(item)));
+        }
+    }
+
+    list
+}
+
+fn add_two_list(
+    list_one: Option<Box<ListNode>>,
+    list_two: Option<Box<ListNode>>,
+) -> Option<Box<ListNode>> {
+    let result = make_number_from_list(list_one) + make_number_from_list(list_two);
+    println!("1*************** result number {}", result);
+
+    let list_number: Vec<usize> = result
+        .to_string()
+        .split("")
+        .map(|x| x.parse::<usize>().unwrap())
+        .collect();
+
+    //let x = list_number;
+
+    println!("2*************** result number ");
+
+    make_list_from_array(list_number)
+}
+
+fn print_list(list: Box<ListNode>) {
+    // print list with given head
+
+    let mut iter_list = Some(list.clone());
+
+    while let Some(iter_list_value) = iter_list {
+        println!("{}", iter_list_value.val);
+        iter_list = iter_list_value.next;
+    }
+}
+
+fn main() {
+    match add_two_list(
+        make_list_from_array(vec![1, 2, 3]),
+        make_list_from_array(vec![2, 3, 4]),
+    ) {
+        Some(value) => {
+            print_list(value);
+        }
+        None => println!("fucked up"),
+    }
 }
