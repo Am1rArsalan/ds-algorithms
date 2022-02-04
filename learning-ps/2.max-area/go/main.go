daf47f6fbba5305cfb290e5a4232b00f0475eae5
AmirAhmadzadeh <ahamadzadeh1378@gmail.com>
2022-02-05 00:26:52 +0330

    removing unncessary comments

--- learning-ps/2.max-area/go/main.go
+++ learning-ps/2.max-area/go/main.go
@@ -4,16 +4,7 @@ import (
 	"fmt"
 )
 
-// problem 2  :
-// you are given an array of positive numbers
-// which each number represents height of a vertical line
-// on  a chart . find two lines which together with the x-axis
-// forms  a container that will hold the greatest amount of water
-// return  the area of the water that would hold
 
-// [2,2,3,4,6 ,8,5]
-//  0 1 2 3 4  5 6
-//  i
 func main() {
 	area := solution([]int{2, 2, 3, 4, 6, 8, 5})
 	fmt.Println(area)
