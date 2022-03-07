a031fd7dcc39a85a4d0a1d51a1018482bcc0c0ce
AmirAhmadzadeh <ahamadzadeh1378@gmail.com>
2022-03-07 23:40:55 +0330

    chore: update min and max heap test cases

--- problems/12.heaps/ts/MinHeap.ts
+++ problems/12.heaps/ts/MinHeap.ts
@@ -17,18 +17,22 @@ export class MinHeap {
         return min;
     }
 
-    buildHeap() {
-        for (let i = Math.floor((this.heap.length - 1) / 2); i >= 0; --i) {
-            this.heapifyDown(i);
-        }
-    }
-
     insert(value: number) {
         this.heap.push(value);
         this.heapifyUp(this.heap.length - 1);
     }
 
-    heapifyUp(index: number) {
+    getHeap() {
+        return this.heap;
+    }
+
+    private buildHeap() {
+        for (let i = Math.floor((this.heap.length - 1) / 2); i >= 0; --i) {
+            this.heapifyDown(i);
+        }
+    }
+
+    private heapifyUp(index: number) {
         let parentIndex = Math.floor((index - 1) / 2); // 0
 
         if (this.heap[parentIndex] > this.heap[index]) {
@@ -39,7 +43,7 @@ export class MinHeap {
         }
     }
 
-    heapifyDown(index: number) {
+    private heapifyDown(index: number) {
         let leftIndex = index * 2 + 1;
         let rightIndex = index * 2 + 2;
         let left = this.heap[leftIndex];
--- problems/12.heaps/ts/index.test.ts
+++ problems/12.heaps/ts/index.test.ts
@@ -1,8 +1,9 @@
-import { describe, it } from 'vitest';
+import { describe, it, assert } from 'vitest';
 import { MaxHeap } from './MaxHeap';
+import { MinHeap } from './MinHeap';
 
 describe('heaps and priority queues', () => {
-    it('...', () => {
+    it('making max heap by getting initial initial incorrect max heap', () => {
         const heap = new MaxHeap([]);
         const heap2 = new MaxHeap([25, 40, 15, 20, 10, 50, 35]);
 
@@ -14,7 +15,23 @@ describe('heaps and priority queues', () => {
         heap.insert(50);
         heap.insert(40);
 
-        console.log('what is the heap1 ?', heap.getHeap());
-        console.log('what is the heap2 ?', heap2.getHeap());
+        assert.deepEqual(heap.getHeap(), [50, 25, 40, 15, 20, 10, 35]);
+        assert.deepEqual(heap2.getHeap(), [50, 40, 35, 20, 10, 15, 25]);
+    });
+
+    it('making min heap by getting initial incorrect min heap', () => {
+        const heap = new MinHeap([]);
+        const heap2 = new MinHeap([25, 40, 15, 20, 10, 50, 35]);
+
+        heap.insert(15);
+        heap.insert(35);
+        heap.insert(10);
+        heap.insert(25);
+        heap.insert(20);
+        heap.insert(50);
+        heap.insert(40);
+
+        assert.deepEqual(heap.getHeap(), [10, 20, 15, 35, 25, 50, 40]);
+        assert.deepEqual(heap2.getHeap(), [10, 20, 15, 25, 40, 50, 35]);
     });
 });
