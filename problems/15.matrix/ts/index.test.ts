0758da5a1db16c477b0b4f3573afd40d28612bd0
AmirAhmadzadeh <ahamadzadeh1378@gmail.com>
2022-03-09 09:23:03 +0330

    chore: working on bfs travese in matrix

--- problems/15.matrix/ts/index.test.ts
+++ problems/15.matrix/ts/index.test.ts
@@ -1,5 +1,8 @@
 import { describe, it, assert } from 'vitest';
-import { traverseTwoDimensionalArray } from './index';
+import {
+    traverseTwoDimensionalArray,
+    bfsTraverseTwoDimensionalArray,
+} from './index';
 
 describe('2 dimensional arrays', () => {
     it('traverse 2 dimensional arrays ( dfs )', () => {
@@ -13,5 +16,17 @@ describe('2 dimensional arrays', () => {
 
         assert.equal(sortedResult.length, 20);
         assert.deepEqual(sortedResult, testCase.flat());
+    });
+
+    it('traverse 2 dimensional arrays ( bfs )', () => {
+        const testCase = Array.from({ length: 4 }, (_, i) => {
+            return Array.from({ length: 5 }, (_, j) => i + j + 1 + 4 * i);
+        });
+        const result = bfsTraverseTwoDimensionalArray<number>(testCase);
+        //result.sort((a, b) => (a > b ? 1 : -1));
+
+        console.log(result, testCase);
+        assert.equal(result.length, 20);
+        assert.deepEqual(result, testCase.flat());
     });
 });
--- problems/15.matrix/ts/index.ts
+++ problems/15.matrix/ts/index.ts
@@ -6,43 +6,94 @@ const directions = [
 ];
 
 export function dfs<T>(
-    arr: T[][],
+    matrix: T[][],
     row: number,
     col: number,
     seen: boolean[][],
     result: T[]
 ): void {
-    if (row < 0 || col < 0 || row >= arr.length || col >= arr[0].length) {
+    if (row < 0 || col < 0 || row >= matrix.length || col >= matrix[0].length) {
         return;
     }
 
-    result.push(arr[row][col]);
+    result.push(matrix[row][col]);
     seen[row][col] = true;
 
     for (let i = 0; i < directions.length; ++i) {
         const direction = directions[i];
         if (
-            arr[row + direction[0]] &&
-            arr[row + direction[0]][col + direction[1]] &&
+            matrix[row + direction[0]] &&
+            matrix[row + direction[0]][col + direction[1]] &&
             !seen[row + direction[0]][col + direction[1]]
         ) {
-            dfs(arr, row + direction[0], col + direction[1], seen, result);
+            dfs(matrix, row + direction[0], col + direction[1], seen, result);
         }
     }
 }
 
-export function traverseTwoDimensionalArray<T extends number>(arr: T[][]) {
+export function traverseTwoDimensionalArray<T>(matrix: T[][]) {
     const seenItems: boolean[][] = Array.from(
-        { length: arr.length },
+        { length: matrix.length },
         (_, index) => {
-            return Array.from<boolean>({ length: arr[index].length }).fill(
+            return Array.from<boolean>({ length: matrix[index].length }).fill(
                 false
             );
         }
     );
     const result: T[] = [];
 
-    dfs(arr, 0, 0, seenItems, result);
+    dfs(matrix, 0, 0, seenItems, result);
+
+    return result;
+}
+
+export function bfsTraverseTwoDimensionalArray<T>(matrix: T[][]) {
+    const seen: boolean[][] = Array.from(
+        { length: matrix.length },
+        (_, index) => {
+            return Array.from<boolean>({ length: matrix[index].length }).fill(
+                false
+            );
+        }
+    );
+    let row = Math.ceil((matrix.length - 1) / 2);
+    let col = Math.ceil((matrix[0].length - 1) / 2);
+    let queue = [matrix[row][col]];
+    let result = [matrix[row][col]];
+
+    while (queue.length > 0) {
+        if (queue.length === 4) {
+            row = row - 1;
+        } else if (queue.length === 3) {
+            col = col + 1;
+        } else if (queue.length === 2) {
+            row = row + 1;
+        } else if (queue.length === 2) {
+            col = col - 1;
+        }
+
+        queue.shift();
+
+        for (let i = 0; i < 4; i++) {
+            const direction = directions[i];
+
+            if (
+                row + direction[0] < 0 ||
+                row + direction[0] >= matrix.length ||
+                col + direction[1] < 0 ||
+                col + direction[1] >= matrix[0].length ||
+                seen[row + direction[0]][col + direction[1]]
+            ) {
+                continue;
+            }
+
+            if (!seen[row + direction[0]][col + direction[1]]) {
+                queue.push(matrix[row + direction[0]][col + direction[1]]);
+                result.push(matrix[row + direction[0]][col + direction[1]]);
+                seen[row + direction[0]][col + direction[1]] = true;
+            }
+        }
+    }
 
     return result;
 }
