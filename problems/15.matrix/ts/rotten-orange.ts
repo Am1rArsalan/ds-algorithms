3440bbef0fa20ad6dd1f63e4387f9b0a885cd8ab
AmirAhmadzadeh <ahamadzadeh1378@gmail.com>
2022-03-13 17:12:49 +0330

    chore: refactor rotten orange solution

--- problems/15.matrix/ts/rotten-orange.ts
+++ problems/15.matrix/ts/rotten-orange.ts
@@ -5,7 +5,7 @@ const directions = [
     [0, -1],
 ];
 
-export function findRottenOranges(matrix: number[][]) {
+export function findRottenOrangesAndCountFresh(matrix: number[][]) {
     let initialRow = Math.ceil((matrix.length - 1) / 2);
     let initialCol = Math.ceil((matrix[0].length - 1) / 2);
     let queue = [[initialRow, initialCol]];
@@ -13,6 +13,7 @@ export function findRottenOranges(matrix: number[][]) {
         Array<boolean>(matrix[0].length).fill(false)
     );
     const result = [];
+    let count = 0;
     if (matrix[initialRow][initialCol] === 2) {
         result.push([initialRow, initialCol]);
     }
@@ -34,53 +35,23 @@ export function findRottenOranges(matrix: number[][]) {
                 seen[row + direction[0]][col + direction[1]] = true;
                 matrix[row + direction[0]][col + direction[1]] === 2 &&
                     result.push([row + direction[0], col + direction[1]]);
-            }
-        }
-    }
 
-    return result;
-}
-
-function countNumberOfFreshOranges(matrix: number[][]) {
-    let initialRow = Math.ceil((matrix.length - 1) / 2);
-    let initialCol = Math.ceil((matrix[0].length - 1) / 2);
-    let queue = [[initialRow, initialCol]];
-    let seen = Array.from({ length: matrix.length }, () =>
-        Array<boolean>(matrix[0].length).fill(false)
-    );
-    let result = 0;
-    if (matrix[initialRow][initialCol] === 1) {
-        result += 1;
-    }
-    seen[initialRow][initialCol] = true;
-
-    while (queue.length > 0) {
-        let coordinate = queue.shift() as [number, number];
-        let row = coordinate[0];
-        let col = coordinate[1];
-        for (let i = 0; i < directions.length; i++) {
-            let direction = directions[i];
-            if (
-                matrix[row + direction[0]] &&
-                matrix[row + direction[0]][col + direction[1]] !== undefined &&
-                !seen[row + direction[0]][col + direction[1]]
-            ) {
-                queue.push([row + direction[0], col + direction[1]]);
-                seen[row + direction[0]][col + direction[1]] = true;
                 if (matrix[row + direction[0]][col + direction[1]] === 1) {
-                    result += 1;
+                    count++;
                 }
             }
         }
     }
 
-    return result;
+    return { q: result, numberOfFreshOranges: count };
 }
 
 export function timeTakeToRotten(matrix: number[][]) {
-    let queue = findRottenOranges(matrix);
+    if (matrix.length === 0) return 0;
+    let queue = findRottenOrangesAndCountFresh(matrix).q;
+    let numberOfFreshOranges =
+        findRottenOrangesAndCountFresh(matrix).numberOfFreshOranges;
     let minutes = 0;
-    let numberOfFreshOranges = countNumberOfFreshOranges(matrix); //
     let queueLength = queue.length;
     while (queue.length > 0) {
         queueLength -= 1;
