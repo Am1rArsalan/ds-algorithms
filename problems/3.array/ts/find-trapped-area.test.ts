b307f8aaf888b789aa2f6f16c52596974de9403f
AmirAhmadzadeh <ahamadzadeh1378@gmail.com>
2022-03-25 09:46:48 +0430

    chore: moving the solutions to own files

---
+++ problems/3.array/ts/find-trapped-area.test.ts
@@ -0,0 +1,16 @@
+import { describe, assert, it } from 'vitest';
+import { findTrappedArea, findTrappedArea2 } from './index';
+
+describe('finding the trapped area between in given head', () => {
+    describe('max trapped water question problem', () => {
+        it('should pass the test', () => {
+            assert.equal(findTrappedArea([0, 1, 0, 2, 1, 0, 3, 1, 0, 1, 2]), 8);
+        });
+        it('optimized findTrappedArea 2', () => {
+            assert.equal(
+                findTrappedArea2([0, 1, 0, 2, 1, 0, 3, 1, 0, 1, 2]),
+                8
+            );
+        });
+    });
+});
