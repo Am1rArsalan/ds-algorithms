8c0bc14b260eff7bc1c67438e8d136299b0ba17c
AmirAhmadzadeh <ahamadzadeh1378@gmail.com>
2022-03-27 13:27:35 +0430

    chore: update solution and use replace all method

--- problems/4.string/ts/index.test.ts
+++ problems/4.string/ts/index.test.ts
@@ -1,16 +1,9 @@
-import { compareTwoString, generateString } from "./index";
-import { describe, it, assert } from "vitest";
+import { compareTwoString, generateString } from './index';
+import { describe, it, assert } from 'vitest';
 
-describe("problem 4", () => {
-  it("should compare two strings", () => {
-    assert.equal(compareTwoString("axeqq#a", "asexq#a"), false);
-    assert.equal(compareTwoString("axeqq#a", "axeqW#a"), true);
-  });
-
-  it("generate string helper should remove the character before '#'", () => {
-    assert.equal(generateString("axeqq#a"), "axeqa");
-    assert.equal(generateString("asexq#a"), "asexa");
-    assert.equal(generateString("axeqq#a"), "axeqa");
-    assert.equal(generateString("axeqW#a"), "axeqa");
-  });
+describe('problem 4', () => {
+    it('should compare two strings', () => {
+        assert.equal(compareTwoString('axeqq#a', 'asexq#a'), false);
+        assert.equal(compareTwoString('axeqq#a', 'axeqW#a'), true);
+    });
 });
--- problems/4.string/ts/index.ts
+++ problems/4.string/ts/index.ts
@@ -1,15 +1,4 @@
-export function generateString(str: string): string {
-  let resultArray: string[] = [];
-  for (let i = 0; i < str.length; ++i) {
-    if (str[i] !== "#") {
-      resultArray.push(str[i]);
-    } else {
-      resultArray.pop();
-    }
-  }
-  return resultArray.join("");
-}
-
 export function compareTwoString(S: string, T: string) {
-  return generateString(S) === generateString(T);
+    const removeRegex = new RegExp(/.#/g);
+    return S.replaceAll(removeRegex, '') === T.replaceAll(removeRegex, '');
 }
