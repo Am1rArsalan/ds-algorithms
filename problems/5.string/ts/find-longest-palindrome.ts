74ca4174dba21ebf41e9305c74257d6f14471d83
AmirAhmadzadeh <ahamadzadeh1378@gmail.com>
2022-04-03 17:01:09 +0430

    feat: longest plaindrome question

---
+++ problems/5.string/ts/find-longest-palindrome.ts
@@ -0,0 +1,32 @@
+function checkPalindrome(str: string): boolean {
+    if (str.length == 0) return true;
+    let R = Math.floor(str.length / 2);
+    let L = str.length % 2 === 0 ? R - 1 : R;
+    let isPalindrome = true;
+
+    while (R < str.length && L >= 0) {
+        if (str[R] !== str[L]) {
+            isPalindrome = false;
+            break;
+        }
+
+        L--;
+        R++;
+    }
+
+    return isPalindrome;
+}
+
+export function longestPalindrome(str: string): string {
+    let max = '';
+    for (let i = 0; i < str.length; i++) {
+        for (let j = i; j < str.length; j++) {
+            let temp = str.substring(i, j + 1);
+            if (checkPalindrome(temp) && temp.length > max.length) {
+                max = temp;
+            }
+        }
+    }
+
+    return max;
+}
