994eb8316ff0188818cfd5dcd76688d6e39b07d0
AmirAhmadzadeh <ahamadzadeh1378@gmail.com>
2022-04-14 23:13:00 +0430

    feat: finish course problem in golang

--- problems/golang/16.graphs/main/finish_course.go
+++ problems/golang/16.graphs/main/finish_course.go
@@ -1,5 +1,47 @@
 package main
 
-func finishCourses() bool {
-	return false
+func detectCycle(adjList [][]int, currentVertex int) bool {
+	queue := adjList[currentVertex]
+	seen := make(map[int]bool, len(adjList))
+	isCycle := false
+
+	for len(queue) > 0 {
+		vertex := queue[0]
+		queue = queue[1:]
+		seen[vertex] = true
+
+		if vertex == currentVertex {
+			isCycle = true
+			break
+		}
+
+		connections := adjList[vertex]
+		for _, connection := range connections {
+			if !seen[connection] {
+				queue = append(queue, connection)
+			}
+		}
+	}
+
+	return isCycle
+}
+
+func canFinishCourses(numOfCourses int, prerequisites [][]int) bool {
+
+	adjList := make([][]int, numOfCourses)
+
+	for _, prerequisite := range prerequisites {
+		adjList[prerequisite[0]] = append(adjList[prerequisite[0]], prerequisite[1])
+	}
+
+	isFinishable := true
+
+	for i := 0; i < numOfCourses; i++ {
+		if detectCycle(adjList, i) {
+			isFinishable = false
+			break
+		}
+	}
+
+	return isFinishable
 }
--- problems/golang/16.graphs/main/finish_course_test.go
+++ problems/golang/16.graphs/main/finish_course_test.go
@@ -3,5 +3,44 @@ package main
 import "testing"
 
 func TestFinishCourse(t *testing.T) {
-	// your test code here
+
+	expectedResult := true
+	result := canFinishCourses(6, [][]int{
+		{1, 0},
+		{2, 1},
+		{2, 5},
+		{0, 3},
+		{4, 3},
+		{3, 5},
+		{4, 5},
+	})
+
+	if !result {
+		t.Errorf("1.Expected (%v) but got"+
+			"(%v)", expectedResult, result)
+	}
+
+	expectedResult = true
+	result = canFinishCourses(4, [][]int{
+		{1, 0},
+		{2, 1},
+		{0, 3},
+	})
+
+	if !result {
+		t.Errorf("2.Expected (%v) but got"+
+			"(%v)", expectedResult, result)
+	}
+
+	expectedResult = false
+	result = canFinishCourses(3, [][]int{
+		{1, 0},
+		{2, 1},
+		{0, 2},
+	})
+
+	if result {
+		t.Errorf("3.Expected (%v) but got"+
+			"(%v)", expectedResult, result)
+	}
 }
