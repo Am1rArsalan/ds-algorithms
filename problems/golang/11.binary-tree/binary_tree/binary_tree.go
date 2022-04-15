4c2860214b133752ff5e8cea21b8105803867b89
AmirAhmadzadeh <ahamadzadeh1378@gmail.com>
2022-04-15 20:21:34 +0430

    feat: add size to binary_tree struct

--- problems/golang/11.binary-tree/binary_tree/binary_tree.go
+++ problems/golang/11.binary-tree/binary_tree/binary_tree.go
@@ -4,19 +4,30 @@ import "github.com/AmirAhmadzadeh/problems/node"
 
 type BinaryTree struct {
 	root *node.Node
+	size int
 }
 
-func New(value int) BinaryTree {
-	return BinaryTree{
+func New(value int) *BinaryTree {
+	return &BinaryTree{
 		root: node.New(value),
+		size: 1,
 	}
 }
 
 func (tree *BinaryTree) Insert(value int) *BinaryTree {
+	tree.size += 1
 	tree.root.Insert(value)
 	return tree
 }
 
 func (tree *BinaryTree) Debug() {
 	tree.root.Debug()
+}
+
+func (tree *BinaryTree) GetRoot() *node.Node {
+	return tree.root
+}
+
+func (tree *BinaryTree) GetSize() int {
+	return tree.size
 }
