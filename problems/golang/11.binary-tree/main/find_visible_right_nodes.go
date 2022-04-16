67c6df474910fae8ef8001f9e327ae4b4607e268
AmirAhmadzadeh <ahamadzadeh1378@gmail.com>
2022-04-16 20:43:31 +0430

    feat: solving right side tree in golang with dfs

--- problems/golang/11.binary-tree/main/find_visible_right_nodes.go
+++ problems/golang/11.binary-tree/main/find_visible_right_nodes.go
@@ -5,10 +5,51 @@ import (
 	"github.com/AmirAhmadzadeh/problems/node"
 )
 
-func findVisibleNodesFromRightSide(tree *binary_tree.BinaryTree) [][]int {
+func dfs(result *[]int, queue *[]*node.Node) []int {
+	if len(*queue) == 0 {
+		return *result
+	}
+
+	queueLength := len(*queue)
+	level := []int{}
+
+	for i := 0; i < queueLength; i++ {
+		vertex := (*queue)[0]
+		*queue = (*queue)[1:]
+		if len(level) == 0 {
+			level = append(level, vertex.Value())
+		}
+
+		if vertex.Right() != nil {
+			*queue = append(*queue, vertex.Right())
+		}
+
+		if vertex.Left() != nil {
+			*queue = append(*queue, vertex.Left())
+		}
+	}
+
+	if len(level) == 1 {
+		*result = append(*result, level[0])
+	}
+
+	return dfs(result, queue)
+}
+
+// bfs
+func findVisibleNodesFromRightSide1(tree *binary_tree.BinaryTree) []int {
+	root := tree.GetRoot()
+	result := []int{}
+	queue := []*node.Node{root}
+
+	return dfs(&result, &queue)
+}
+
+// bfs
+func findVisibleNodesFromRightSide2(tree *binary_tree.BinaryTree) []int {
 	root := tree.GetRoot()
 	queue := []*node.Node{root}
-	result := [][]int{}
+	result := []int{}
 
 	for len(queue) > 0 {
 		queueLength := len(queue)
@@ -31,8 +72,8 @@ func findVisibleNodesFromRightSide(tree *binary_tree.BinaryTree) [][]int {
 
 		}
 
-		if len(level) > 0 {
-			result = append(result, level)
+		if len(level) == 1 {
+			result = append(result, level[0])
 		}
 	}
 
--- problems/golang/11.binary-tree/main/find_visible_right_nodes_test.go
+++ problems/golang/11.binary-tree/main/find_visible_right_nodes_test.go
@@ -1,27 +1,42 @@
 package main
 
 import (
-	"fmt"
 	"github.com/AmirAhmadzadeh/problems/binary_tree"
 	"reflect"
 	"testing"
 )
 
-func TestVisibleNodesFromRightSide(t *testing.T) {
+// dfs
+func TestVisibleNodesFromRightSide1(t *testing.T) {
 	binaryTree := binary_tree.New(3)
 	root := binaryTree.GetRoot()
 	root.PushRightLeaf(1).PushRightLeaf(4)
 	generatedNode := root.PushLeftLeaf(6)
 	generatedNode.PushRightLeaf(2)
 	generatedNode.PushLeftLeaf(9).PushRightLeaf(5).PushLeftLeaf(8)
-	result := findVisibleNodesFromRightSide(binaryTree)
+	result := findVisibleNodesFromRightSide1(binaryTree)
 
-	//expectedResult          [[3] [6 1]  [9 2 4] [5] [8]]
-	expectedResult := [][]int{{3}, {6, 1}, {9, 2, 4}, {5}, {8}}
+	expectedResult := []int{3, 1, 4, 5, 8}
 
-	fmt.Println("expectedResult", expectedResult)
+	if !reflect.DeepEqual(expectedResult, result) {
+		t.Errorf("Expected (%v) is not same as"+
+			" actual string (%v)", expectedResult, result)
+	}
+}
 
-	if reflect.DeepEqual(expectedResult, result) {
+// bfs
+func TestVisibleNodesFromRightSide2(t *testing.T) {
+	binaryTree := binary_tree.New(3)
+	root := binaryTree.GetRoot()
+	root.PushRightLeaf(1).PushRightLeaf(4)
+	generatedNode := root.PushLeftLeaf(6)
+	generatedNode.PushRightLeaf(2)
+	generatedNode.PushLeftLeaf(9).PushRightLeaf(5).PushLeftLeaf(8)
+	result := findVisibleNodesFromRightSide2(binaryTree)
+
+	expectedResult := []int{3, 1, 4, 5, 8}
+
+	if !reflect.DeepEqual(expectedResult, result) {
 		t.Errorf("Expected (%v) is not same as"+
 			" actual string (%v)", expectedResult, result)
 	}
