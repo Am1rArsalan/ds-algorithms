6065580c22e04ca5ec806df5e6c599a3c36054c4
AmirAhmadzadeh <ahamadzadeh1378@gmail.com>
2022-04-18 07:46:33 +0430

    chore: update dfs traversal in find visible right side nodes

--- problems/golang/11.binary-tree/main/find_visible_right_nodes.go
+++ problems/golang/11.binary-tree/main/find_visible_right_nodes.go
@@ -5,44 +5,29 @@ import (
 	"github.com/AmirAhmadzadeh/problems/node"
 )
 
-func dfs(result *[]int, queue *[]*node.Node) []int {
-	if len(*queue) == 0 {
-		return *result
+func dfs(node *node.Node, level int, result *[]int) {
+	//kill condition
+	if node == nil {
+		return
 	}
 
-	queueLength := len(*queue)
-	level := []int{}
-
-	for i := 0; i < queueLength; i++ {
-		vertex := (*queue)[0]
-		*queue = (*queue)[1:]
-		if len(level) == 0 {
-			level = append(level, vertex.Value())
-		}
-
-		if vertex.Right() != nil {
-			*queue = append(*queue, vertex.Right())
-		}
-
-		if vertex.Left() != nil {
-			*queue = append(*queue, vertex.Left())
-		}
+	if node.Right() != nil {
+		dfs(node.Right(), level+1, result)
 	}
 
-	if len(level) == 1 {
-		*result = append(*result, level[0])
+	if node.Left() != nil {
+		dfs(node.Left(), level+1, result)
 	}
-
-	return dfs(result, queue)
 }
 
-// bfs
+// dfs
 func findVisibleNodesFromRightSide1(tree *binary_tree.BinaryTree) []int {
 	root := tree.GetRoot()
 	result := []int{}
-	queue := []*node.Node{root}
 
-	return dfs(&result, &queue)
+	dfs(root, 0, &result)
+
+	return result
 }
 
 // bfs
