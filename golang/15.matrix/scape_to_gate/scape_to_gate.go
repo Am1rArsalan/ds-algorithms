ea396153cae5a82fa128fe4f1fbaae11e858850b
AmirAhmadzadeh <ahamadzadeh1378@gmail.com>
2022-04-22 22:58:54 +0430

    feat: solve scape_to_gate problem with dfs

--- golang/15.matrix/scape_to_gate/scape_to_gate.go
+++ golang/15.matrix/scape_to_gate/scape_to_gate.go
@@ -11,6 +11,71 @@ var directions = [][]int{
 	{0, -1},
 }
 
+func dfs(matrix *[][]int, row, col, steps int) {
+	if row >= len(*matrix) || row < 0 || col >= len((*matrix)[0]) || col < 0 {
+		return
+	}
+
+	if (*matrix)[row][col] < steps {
+		return
+	}
+
+	(*matrix)[row][col] = steps
+
+	for _, direction := range directions {
+		directionRow := direction[0] + row
+		directionCol := direction[1] + col
+
+		if directionRow < len(*matrix) && directionRow >= 0 && directionCol < len((*matrix)[0]) && directionCol >= 0 {
+			if (*matrix)[directionRow][directionCol] != Wall && (*matrix)[directionRow][directionCol] != Gate {
+				dfs(matrix, directionRow, directionCol, steps+1)
+			}
+		}
+	}
+}
+
+func scapeToGateDfs(matrix [][]int) [][]int {
+	seen := [][]bool{}
+
+	for i := 0; i < len(matrix); i++ {
+		seen = append(seen, []bool{})
+		for j := 0; j < len((matrix)[i]); j++ {
+			seen[i] = append(seen[i], false)
+		}
+	}
+
+	queue := [][]int{{0, 0}}
+
+	for len(queue) > 0 {
+		current := queue[0]
+		queue = queue[1:]
+		row := current[0]
+		col := current[1]
+
+		seen[row][col] = true
+
+		if matrix[row][col] == Gate {
+			dfs(&matrix, row, col, 0)
+		}
+
+		for _, direction := range directions {
+			tempRow := direction[0] + row
+			tempCol := direction[1] + col
+
+			if tempRow < len(matrix) && tempRow >= 0 && tempCol < len((matrix)[0]) && tempCol >= 0 {
+				if !seen[tempRow][tempCol] {
+					queue = append(queue, []int{tempRow, tempCol})
+				}
+
+			}
+		}
+
+	}
+
+	return matrix
+}
+
+// Bfs
 func findGates(matrix *[][]int) [][]int {
 	seen := [][]bool{}
 	result := [][]int{}
@@ -53,7 +118,7 @@ func findGates(matrix *[][]int) [][]int {
 	return result
 }
 
-func scapeToGate(matrix [][]int) [][]int {
+func scapeToGateBfs(matrix [][]int) [][]int {
 	queue := findGates(&matrix)
 	queueLength := len(queue)
 	steps := 1
--- golang/15.matrix/scape_to_gate/scape_to_gate_test.go
+++ golang/15.matrix/scape_to_gate/scape_to_gate_test.go
@@ -7,9 +7,34 @@ import (
 	"time"
 )
 
-func TestScapeToGate(t *testing.T) {
+func TestScapeToGateDfs(t *testing.T) {
 	start := time.Now()
-	result := scapeToGate([][]int{
+	result := scapeToGateDfs([][]int{
+		{Empty, Wall, Gate, Empty},
+		{Empty, Empty, Empty, Wall},
+		{Empty, Wall, Empty, Wall},
+		{Gate, Wall, Empty, Empty},
+	})
+
+	expectedResult := [][]int{
+		{3, -1, 0, 1},
+		{2, 2, 1, -1},
+		{1, -1, 2, -1},
+		{0, -1, 3, 4},
+	}
+	timeOfExecution := time.Since(start)
+	log.Printf("1.rot oranges execution took %s", timeOfExecution)
+
+	if !reflect.DeepEqual(expectedResult, result) {
+		t.Errorf("Expected (%v) but got\n"+
+			"  (%v) \n", expectedResult, result)
+	}
+
+}
+
+func TestScapeToGateBfs(t *testing.T) {
+	start := time.Now()
+	result := scapeToGateBfs([][]int{
 		{Empty, Wall, Gate, Empty},
 		{Empty, Empty, Empty, Wall},
 		{Empty, Wall, Empty, Wall},
