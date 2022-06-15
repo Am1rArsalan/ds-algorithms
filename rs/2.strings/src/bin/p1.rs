1406929c3ce85ebca6fafdc1a490a70a2734c945
AmirAhmadzadeh <ahamadzadeh1378@gmail.com>
2022-06-15 10:29:58 +0430

    feat: string manipulation with regex in rust

--- rs/2.strings/src/bin/p1.rs
+++ rs/2.strings/src/bin/p1.rs
@@ -5,43 +5,41 @@ fn main() {
 }
 
 fn compare_two_string_with_regex(first: &str, second: &str) -> bool {
-    //
+    let re = Regex::new(".#").unwrap();
+
+    let first = re.replace_all(first, "".to_string()).to_string();
+    let second = re.replace_all(second, "".to_string()).to_string();
+
+    println!("first ,{:?}", first);
+    println!("second ,{:?}", second);
+
+    return first.eq(&second);
+}
+
+fn pars_text(chars_collection: Vec<char>) -> String {
+    let mut result: Vec<char> = vec![];
+    chars_collection
+        .iter()
+        .enumerate()
+        .for_each(|(index, character)| {
+            if index + 1 < chars_collection.len()
+                && chars_collection[index + 1] != '#'
+                && chars_collection[index] != '#'
+            {
+                result.push(*character);
+            }
+        });
+
+    let result: String = result.into_iter().collect();
+    return result;
 }
 
 fn compare_two_string(first: &str, second: &str) -> bool {
-    let mut first_result: Vec<char> = vec![];
-
     let first_collection: Vec<char> = first.to_string().chars().collect();
-
-    first_collection
-        .iter()
-        .enumerate()
-        .for_each(|(index, character)| {
-            if index + 1 < first_collection.len()
-                && first_collection[index + 1] != '#'
-                && first_collection[index] != '#'
-            {
-                first_result.push(*character);
-            }
-        });
-
-    let mut second_result: Vec<char> = vec![];
     let second_collection: Vec<char> = second.to_string().chars().collect();
 
-    second_collection
-        .iter()
-        .enumerate()
-        .for_each(|(index, character)| {
-            if index + 1 < second_collection.len()
-                && second_collection[index + 1] != '#'
-                && second_collection[index] != '#'
-            {
-                second_result.push(*character);
-            }
-        });
-
-    let second_result: String = second_result.into_iter().collect();
-    let first_result: String = first_result.into_iter().collect();
+    let first_result: String = pars_text(first_collection);
+    let second_result: String = pars_text(second_collection);
 
     return first_result.eq(&second_result);
 }
@@ -50,6 +48,7 @@ fn compare_two_string(first: &str, second: &str) -> bool {
 mod tests {
 
     use crate::compare_two_string;
+    use crate::compare_two_string_with_regex;
 
     #[test]
     fn first_compare() {
@@ -59,7 +58,19 @@ mod tests {
 
     #[test]
     fn second_compare() {
-        let result = compare_two_string("axeqq#a", "axeqW#");
+        let result = compare_two_string("axeqq#a", "axeqW#a");
+        assert_eq!(result, true);
+    }
+
+    #[test]
+    fn first_compare_with_regex() {
+        let result = compare_two_string_with_regex("axeqq#a", "asexq#a");
+        assert_eq!(result, false);
+    }
+
+    #[test]
+    fn second_compare_with_regex() {
+        let result = compare_two_string_with_regex("axeqq#a", "axeqW#a");
         assert_eq!(result, true);
     }
 }
