4f3d0613638d67182eda870fc89bb241de5b9bc8
AmirAhmadzadeh <ahamadzadeh1378@gmail.com>
2022-07-21 20:05:46 +0430

    feat: find closest number to target in dfs

--- ts/11.binary-tree/find-closest-value.test.ts
+++ ts/11.binary-tree/find-closest-value.test.ts
@@ -2,7 +2,7 @@ import { assert, describe, test } from 'vitest';
 import { BinaryTreeImpl } from './BinaryTree';
 import { NodeImpl } from './NodeImpl';
 import { Node } from './Node';
-import { findClosestNumber } from './find-closest-value';
+import { findClosestNumber, findClosestNumberDfs } from './find-closest-value';
 
 describe('find closest value', () => {
     test('first test case', () => {
@@ -16,6 +16,21 @@ describe('find closest value', () => {
         right.pushLeftLeaf(13).pushRightLeaf(14);
 
         let result = findClosestNumber(binaryTree, 12);
+
+        assert.equal(result, 13);
+    });
+
+    test('second test case', () => {
+        let binaryTree = new BinaryTreeImpl<number>(new NodeImpl(10));
+        let root = binaryTree.getRoot() as Node<number>;
+        let right = root.pushRightLeaf(15);
+        let left = root.pushLeftLeaf(5);
+        left.pushRightLeaf(5);
+        left.pushLeftLeaf(2).pushLeftLeaf(1);
+        right.pushRightLeaf(22);
+        right.pushLeftLeaf(13).pushRightLeaf(14);
+
+        let result = findClosestNumberDfs(binaryTree, 12);
 
         assert.equal(result, 13);
     });
--- ts/11.binary-tree/find-closest-value.ts
+++ ts/11.binary-tree/find-closest-value.ts
@@ -9,8 +9,8 @@ export function findClosestNumber(bt: BinaryTree<number>, target: number) {
     while (queue.length > 0) {
         const node = queue.shift() as Node<number>;
 
-        node.left && queue.push(node.left);
-        node.right && queue.push(node.right);
+        node.left && target < node.value && queue.push(node.left);
+        node.right && target > node.value && queue.push(node.right);
 
         if (!seen.get(node.value)) {
             seen.set(node.value, true);
@@ -21,4 +21,29 @@ export function findClosestNumber(bt: BinaryTree<number>, target: number) {
     }
 
     return closest;
+}
+
+function dfs(
+    node: Node<number> | null,
+    target: number,
+    closest: number
+): number {
+    if (!node) return closest;
+
+    // do stuff with target
+    if (Math.abs(target - node.value) < Math.abs(target - closest)) {
+        closest = node.value;
+    }
+
+    if (node.right && target > node.value) {
+        return dfs(node.right, target, closest);
+    } else if (node.left && target < node.value) {
+        return dfs(node.left, target, closest);
+    } else {
+        return closest;
+    }
+}
+
+export function findClosestNumberDfs(bt: BinaryTree<number>, target: number) {
+    return dfs(bt.getRoot(), target, Infinity);
 }
