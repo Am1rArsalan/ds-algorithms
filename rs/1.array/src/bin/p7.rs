dba324845a1b754f2f544d5654702cae14bec542
AmirAhmadzadeh <ahamadzadeh1378@gmail.com>
2022-08-08 18:34:07 +0430

    feat: find non constructible change in rustlang

---
+++ rs/1.array/src/bin/p7.rs
@@ -0,0 +1,31 @@
+fn main() {
+    todo!();
+}
+
+fn find_non_constructible_change(arr: &mut Vec<i32>) -> i32 {
+    let mut change = 0;
+
+    arr.sort();
+
+    for value in arr {
+        if change + 1 < *value {
+            break;
+        }
+
+        change = change + *value;
+    }
+
+    change + 1
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::find_non_constructible_change;
+
+    #[test]
+    fn validate_subsequence_test_1() {
+        let mut arr = vec![5, 7, 1, 1, 2, 3, 22];
+        let result = find_non_constructible_change(&mut arr);
+        assert_eq!(result, 20);
+    }
+}
