eb1a4d2cd8cc4ef390107cb4a6883adf7e116aae
Am1rArsalan <amirahmadzadehdev@gmail.com>
2024-03-30 09:55:54 +0100

    feat: solve problem 17;

--- golang/1.array/problem17.go
+++ golang/1.array/problem17.go
@@ -1,15 +1,26 @@
 package main
 
 func RearrangeArrayAlternativelyWithoutExtraSpace(a []int) []int {
-	t := len(a) - 1
-	c := 0
 
-	for i := 0; i+2 < len(a); i += 2 {
-		a[i], a[t] = a[t], a[i]
-		a[i+1], a[t+c] = a[t+c], a[i+1]
+	E := len(a) - 1
+	S := 0
+	ME := a[E] + 1
 
-		c++
-		t--
+	// encoding
+	for i := 0; i < len(a); i++ {
+		if i%2 == 0 && E >= 0 {
+			// even => max
+			a[i] = ((a[E] % ME) * ME) + a[i]
+			E--
+		} else if S < len(a) {
+			// odd => min
+			a[i] = ((a[S] % ME) * ME) + a[i]
+			S++
+		}
+	}
+
+	for i := 0; i < len(a); i++ {
+		a[i] = a[i] / ME
 	}
 
 	return a
