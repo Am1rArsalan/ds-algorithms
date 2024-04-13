0edc5b160e1f1935b095101fff6db1b42b373ae9
Am1rArsalan <amirahmadzadehdev@gmail.com>
2024-04-13 23:08:59 +0200

    feat: problem 18;

--- golang/1.array/problem18.go
+++ golang/1.array/problem18.go
@@ -1,7 +1,32 @@
-package main 
+package main
 
+import (
+	"sort"
+)
 
+func CountPairs(X, Y []int, M, N int) int64 {
+	var ans int64 = 0
 
-func FindNumberOfPairs(a []int) int { 
-	return 0 
-} 
+	sort.Ints(Y) 
+
+	for i := 0; i < M; i++ {
+		switch X[i] {
+		case 1:
+			continue
+
+		case 2:
+			ans += int64(sort.SearchInts(Y, 2))     
+			ans += int64(N - sort.SearchInts(Y, 5))
+
+		default:
+			ans += int64(N - sort.SearchInts(Y, X[i]+1))        
+			ans += int64(sort.SearchInts(Y, 2) - sort.SearchInts(Y, 1)) 
+
+			if X[i] == 3 {
+				ans += int64(sort.SearchInts(Y, 3) - sort.SearchInts(Y, 2))
+			}
+		}
+	}
+
+	return ans
+}
--- golang/1.array/problem18_test.go
+++ golang/1.array/problem18_test.go
@@ -4,21 +4,24 @@ import (
 	"testing"
 )
 
-func TestFindNumberOfPairs(t *testing.T) {
-	r := FindNumberOfPairs([]int{2, 1, 6})
-	er := 3 
+func TestCountPairs(t *testing.T) {
+	a := []int{2, 1, 6}
+	b := []int{1, 5}
+	r := CountPairs(a, b, len(a), len(b))
+	var er int64 = 3
 
-	if r != er { 
-		t.Log("First(1) test case")
+	if r != er {
+		t.Log("First test case")
 		t.Errorf("expected: %+v, got : %+v", er, r)
 	}
 
+	a = []int{2, 3, 4, 5}
+	b = []int{1, 2, 3}
+	r1 := CountPairs(a, b, len(a), len(b))
+	var er1 int64 = 5
 
-	r1 := FindNumberOfPairs([]int{2 ,3, 4, 5})
-	er1 := 5 
-
-	if r1 != er1 { 
-		t.Log("First(1) test case")
+	if r1 != er1 {
+		t.Log("Second test case")
 		t.Errorf("expected: %+v, got : %+v", er1, r1)
 	}
 }
